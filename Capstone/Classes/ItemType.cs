50c5bd02dd0f03754fc65d3b688efaac0cda8ec5
William Lawrence <williamjlawrence1@gmail.com>
2018-06-14 10:23:22 -0400

    added VendingMachineItem abstract class and ItemType enum

---
+++ Capstone/Classes/ItemType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    /// <summary>
+    /// Represents the item type
+    /// </summary>
+    public enum ItemType
+    {
+        Candy,
+        Chips,
+        Drink,  
+        Gum
+    }
+}
---
+++ Capstone/Classes/VendingMachingItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    public abstract class VendingMachingItem
+    {
+        /// <summary>
+        /// Slot where the item is stored
+        /// </summary>
+        public string SlotLocation { get; private set; }
+
+        /// <summary>
+        /// Name of the product
+        /// </summary>
+        public string ProductName { get; private set; }
+
+        /// <summary>
+        /// Price of the product.
+        /// </summary>
+        public decimal Price { get; private set; }
+
+        /// <summary>
+        /// Type of the product.
+        /// </summary>
+        public ItemType ItemType { get; private set; }
+
+        /// <summary>
+        /// Quantity of the product.
+        /// </summary>
+        public int Quantity { get; private set; }
+
+
+        public VendingMachingItem(string slotLocation, string productName, decimal price, ItemType itemType )
+        {
+            this.SlotLocation = slotLocation;
+            this.ProductName = productName;
+            this.Price = price;
+            this.ItemType = itemType;
+        }
+
+
+        /// <summary>
+        /// The sound made when the product is consumed.
+        /// </summary>
+        /// <returns>The sound made by a particular item type</returns>
+        public abstract string MakeConsumptionSound();
+
+    }
+}
